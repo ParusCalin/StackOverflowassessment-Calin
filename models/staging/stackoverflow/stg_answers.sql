4d26644bf296404e4b68b0e9cd3500489df4ed31
parus_calin@yahoo.com <parus_calin@yahoo.com>
2025-09-29 19:48:07 +0000

    fixing

--- models/staging/stackoverflow/stg_answers.sql
+++ models/staging/stackoverflow/stg_answers.sql
@@ -1,9 +1,21 @@
+-- Keep only answers whose parent question exists in questions_raw
 {{ config(materialized='view') }}
 
+with a as (
+  select
+    id          as answer_id,
+    parent_id   as question_id,
+    creation_date as answer_created_at,
+    score       as answer_score,
+    owner_user_id
+  from {{ source('so','answers_raw') }}
+),
+q as (
+  select id as question_id
+  from {{ source('so','questions_raw') }}
+)
+
 select
-  id as answer_id,
-  parent_id as question_id,
-  creation_date as answer_created_at,
-  score as answer_score,
-  owner_user_id
-from {{ source('so','answers_raw') }}
+  a.*
+from a
+join q using (question_id)
